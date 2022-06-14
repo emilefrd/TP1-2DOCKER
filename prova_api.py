ba0ea97c85ef79787984aa7cec423de5c9d083a7
emilefrd <92552059+emilefrd@users.noreply.github.com>
2022-06-14 19:37:33 +0200

    Update prova_api.py

--- prova_api.py
+++ prova_api.py
@@ -1,13 +1,42 @@
-import  requests
-import  os
+from flask import request, jsonify, Flask
+import os, requests
+from datetime import datetime, timedelta
 
-APIKEY = os.environ['APIKEY'] # reads the environment variable
-LAT = os.environ['LAT']
-LONG = os.environ['LONG']
+app = Flask(__name__)
+app.config.from_object('config.Config')
 
-url="https://api.openweathermap.org/data/2.5/weather?lat="+LAT+"&lon="+LONG+"appid="+APIKEY
-# print(url)
-response = requests.get("https://api.openweathermap.org/data/2.5/weather?lat="+LAT+"&lon="+LONG+"&appid="+APIKEY)
+@app.route('/', methods=['GET'])
+def home():
+    uri = f"https://api.openweathermap.org/data/2.5/weather?lat={app.config['LAT']}&lon={app.config['LONG']}&appid={app.config['APIKEY']}&units=m"
+    res = requests.get(uri)
+    if res.status_code == 200:
 
-print(response.status_code)
-print(response.json())
+        data = res.json()
+        sunrise = datetime.fromtimestamp(data['sys']['sunrise'])
+        sunset = datetime.fromtimestamp(data['sys']['sunset'])
+        latitude = data['coord']['lat']
+        longitude = data['coord']['lon']
+        city = data['name']
+        durée = sunset-sunrise
+        meteo = data['weather'][0]['main']
+        description = data['weather'][0]['description']
+        temp = data['main']['temp']
+        ressentie = data['main']['feels_like']
+        vent = data['wind']['speed']
+        pays = data['sys']['country']
+
+        html = """
+            <p><b> Latitude </b> : """+ str(latitude) +""" °</p>
+            <p><b> Longitude </b> : """+ str(longitude) +""" °</p>
+            <p><b> Pays </b> : """+ str(pays) +"""</p>
+            <p><b> Ville </b> : """+ str(city) +"""</p>
+            <p><b> Température </b> :"""+ str(temp) +""" °C</p>
+            <p><b> Température ressentie </b> : """+ str(ressentie) +""" °C</p>
+            <p><b> Temps </b> : """+ str(meteo) +"""</p>
+            <p><b> Description du temps </b> : """+ str(description) +"""</p>
+            <p><b> Force du vent </b> : """+ str(vent) +"""</p>
+            <p><b> Heure du lever du soleil </b> : """+ str(sunrise) +"""</p>
+            <p><b> Heure du coucher du soleil </b> : """+ str(sunset) +"""</p>
+            <p><b> Durée de soleil </b> : """+ str(durée) +"""</p>"""
+            
+    return html
